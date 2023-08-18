a9cac52f266bf8534d224dde0bd6d1210e80f03f
islamnabila <81310064+islamnabila@users.noreply.github.com>
2023-08-18 19:53:38 +0600

    new file added

--- lib/assignment_module3.dart
+++ lib/assignment_module3.dart
@@ -34,12 +34,12 @@ class Car {
 void main(){
   Car car1 = Car('Toyota','Camry',2020);
   car1.drive(10000);
-  print("Car 1: ${car1.getBrand()} ${car1.getModel()} ${car1.getYear()} Miles: ${car1.getMilesDriven().toStringAsFixed(0)}Age: ${car1.getAge()} ");
+  print("Car 1: ${car1.getBrand()} ${car1.getModel()} ${car1.getYear()} Miles: ${car1.getMilesDriven().toStringAsFixed(0)} Age: ${car1.getAge()} ");
 
   Car car2 = Car('Honda','Civic',2018);
   car2.drive(8000);
   print("Car 2: ${car2.getBrand()} ${car2.getModel()} ${car2.getYear()} Miles: ${car2.getMilesDriven().toStringAsFixed(0)} Age: ${car2.getAge()} ");
-  
+
   Car car3 = Car('Ford','F-150',2015);
   car3.drive(15000);
   print("Car 3: ${car3.getBrand()} ${car3.getModel()} ${car3.getYear()} Miles: ${car3.getMilesDriven().toStringAsFixed(0)} Age: ${car3.getAge()} ");
